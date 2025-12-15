1ba595b486f17240fae60f2b85f3b30fa344f0b7
Shiv <shiv@example.com>
2025-12-15 19:07:27 +0530

    Vercel deploy config + static handling

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ALLOWED_HOSTS + ['.vercel.app'] if 'ALLOWED_HOSTS' in globals() else ['.vercel.app']
 
 
 STATICFILES_DIRS=[
